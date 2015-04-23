fef1b9c17702101e5fa4c80382ad28f564920512
Ubuntu <ubuntu@ip-172-31-20-45.us-west-2.compute.internal>
2015-04-23 02:13:36 +0000

    Added CAS login and logout. WORKING!

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,5 @@
 class StaticPagesController < ApplicationController
+  before_filter CASClient::Frameworks::Rails::Filter
   def home
   end
 
@@ -9,6 +10,10 @@ class StaticPagesController < ApplicationController
   end
 
   def contact
+  end
+
+  def logout
+   CASClient::Frameworks::Rails::Filter.logout(self)
   end
 
 end
